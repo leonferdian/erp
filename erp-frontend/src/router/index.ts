c6d43e06fe3ecae320deac9367ab35ab2ca4c141
leon ferdian <leonardoferdiano@gmail.com>
2025-09-02 08:24:51 +0700

    update add docker

--- erp-frontend/src/router/index.ts
+++ erp-frontend/src/router/index.ts
@@ -1,12 +1,28 @@
-import { createRouter, createWebHistory } from "vue-router";
-import type { RouteRecordRaw } from "vue-router";
-import Login from "../views/Login.vue";
-import Dashboard from "../views/Dashboard.vue";
+import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+// import Login from "../views/Login.vue";
+// import Dashboard from "../views/Dashboard.vue";
 
-const routes = [
-  { path: "/login", name: "Login", component: Login },
-  { path: "/", redirect: "/login" },
-  { path: "/dashboard", component: Dashboard }
+// const routes = [
+//   { path: "/login", name: "Login", component: Login },
+//   { path: "/", redirect: "/login" },
+//   { path: "/dashboard", component: Dashboard }
+// ];
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    redirect: "/login",
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
+  },
+   {
+    path: "/dashboard",
+    name: "Home",
+    component: () => import("../views/Dashboard.vue"),
+  },
 ];
 
 const router = createRouter({
